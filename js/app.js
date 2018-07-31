4c272bf21cf20711dc0e3683693c120ae75e8d46
Brix <Brix.angeles@gmail.com>
2018-07-31 10:07:29 -0700

    added click event to open card

--- js/app.js
+++ js/app.js
@@ -17,8 +17,13 @@ const cardsContainer = document.querySelector(".deck");
 for (let i = 0; i < icons.length; i++) {
     const card = document.createElement("li");
     card.classList.add("card");
-    card.innerHTML = `<i class=${icons[i]}></i>`;
+    card.innerHTML = `<i class="${icons[i]}"></i>`;
     cardsContainer.appendChild(card);
+
+    // click event
+    card.addEventListener("click", function() {
+        card.classList.add("open", "show");
+    });
 }
 
 
